9e68a7a624d1235e5dda7b76734093cc4c02cce1
Yacine-Goumidi <yacine.zeref@gmail.com>
2026-01-29 22:08:59 +0100

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+    panel.addEventListener('click', (e) => {
+        e.preventDefault(); // empêche la navigation immédiate
+
+        removeActiveClasses();
+        panel.classList.add('active');
+
+        const target = panel.getAttribute('href');
+
+        // attend 400ms pour voir l'animation avant navigation
+        setTimeout(() => {
+            window.location.href = target;
+        }, 400);
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+}
