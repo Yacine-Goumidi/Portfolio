c3baa480209153eee8c9ea9ec613ff7f93e47be1
Yacine-Goumidi <yacine.zeref@gmail.com>
2026-02-03 18:19:12 +0100

    UpdateBuildGame

---
+++ HTML/Template/GD/TemplateProjectGameDesign.js
@@ -0,0 +1,17 @@
+﻿const playBtn = document.getElementById("playBtn");
+const gameSection = document.getElementById("gameSection");
+const fullscreenBtn = document.getElementById("fullscreenBtn");
+const unityFrame = document.getElementById("unityFrame");
+
+playBtn.addEventListener("click", () => {
+    gameSection.classList.remove("hidden");
+    gameSection.scrollIntoView({ behavior: "smooth" });
+});
+
+fullscreenBtn.addEventListener("click", () => {
+    if (unityFrame.requestFullscreen) {
+        unityFrame.requestFullscreen();
+    } else if (unityFrame.webkitRequestFullscreen) {
+        unityFrame.webkitRequestFullscreen();
+    }
+});
